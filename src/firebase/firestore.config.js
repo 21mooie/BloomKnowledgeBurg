88a2ae297fcc1df015627fb4d6a903cda10f4b6c
Muata Nkosi <muatankosi1@gmail.com>
2023-05-19 12:32:38 -0400

    Finalize firebase implementation

--- src/firebase/firestore.config.js
+++ src/firebase/firestore.config.js
@@ -1,6 +1,13 @@
 import app from './firebase.config';
 import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 
 const db = getFirestore(app);
 
-export default db;
+const firestoreDB = {
+    addDoc: async function(collectionName, document){
+        return await addDoc(collection(db, collectionName), document);
+    }
+}
+
+export default firestoreDB;
