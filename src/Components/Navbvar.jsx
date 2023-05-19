27952621d9610a5d95bfda5752a46d707dfa1f3d
Robert Quartey <robertquartey@Roberts-MacBook-Air.local>
2023-05-19 10:46:44 -0400

    .

---
+++ src/Components/Navbvar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbvar() {
+  return (
+    <div>Navbvar</div>
+  )
+}
+
+export default Navbvar
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
