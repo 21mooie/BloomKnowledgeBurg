f2e953db380c34a48c4d0ed579f803e96c5b4873
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-05-20 17:07:15 -0400

    for your review-- footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbvar from "./Components/Navbvar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
@@ -9,8 +10,10 @@ const App = () => {
       <Navbvar />
 
       <Outlet />
+      <Footer />
     </div>
   );
 };
 
 export default App;
+
