592020164e4cd8f0cc7e99d7008ea280bc6c80fe
Muata Nkosi <muatankosi1@gmail.com>
2023-05-20 15:35:17 -0400

    Add styling changes for navbar

--- src/Components/Navbvar.jsx
+++ src/Components/Navbvar.jsx
@@ -7,55 +7,64 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import "./Navbvar.css";
 
 const Navbvar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">
-          {" "}
-          <img src={logo} alt="" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Link to={`/`}>
-              <Nav.Link>Home</Nav.Link>
-            </Link>
+    <div>
+      <Form className="d-flex justify-content-end">
+        <div className="d-flex me-3 mt-3">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </div>
+      </Form>
+      <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            {" "}
+            <img src={logo} alt="" className="size"/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mt-3"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Link to={`/`} className="link">
+                <Nav.Link href="#general">Home</Nav.Link>
+              </Link>
 
-            <Link to={`/forum/general`}>
-              <Nav.Link href="#general">Posts</Nav.Link>
-            </Link>
-            <Nav.Link href="#mentors">Mentors</Nav.Link>
-            <Nav.Link href="#action2">Coding </Nav.Link>
-            <Nav.Link href="/jobs"> Jobs </Nav.Link>
+              <Link to={`/jobs`} className="link">
+                <Nav.Link href="#general">Jobs</Nav.Link>
+              </Link>
+              <Link to={`/`} className="link">
+                <Nav.Link href="#mentors">Mentorship</Nav.Link>
+              </Link>
+              <Link to={`/`} className="link">
+                <Nav.Link href="#action2"> Resources </Nav.Link>
+              </Link>
+              <Link to={`/`} className="link">
+                <Nav.Link href="#action2">Meet-Ups </Nav.Link>
+              </Link>
 
-            <Nav.Link href="#action2"> Resources </Nav.Link>
-            <Nav.Link href="#action2">Meet-Ups </Nav.Link>
-
-            <NavDropdown title="More" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Help</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Contact Us</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">About</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <NavDropdown title="More" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Help</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Contact Us</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">About</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+    
   );
 };
 
