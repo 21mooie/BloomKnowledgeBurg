12dbb4ef6ff17f291d34ffd2a37d31762f594da1
Muata Nkosi <muatankosi1@gmail.com>
2023-05-20 14:19:54 -0400

    Add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbvar from "./Components/Navbvar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbvar />
-
       <Outlet />
+      <Footer />
     </div>
   );
 };
