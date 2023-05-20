f55e784dc835b2d202bb88db964fcace3e3e1ddc
Jinsol Kim <37858137+jinsolkim-git@users.noreply.github.com>
2023-05-20 14:36:45 -0400

    add cards

--- src/Pages/Mentors.jsx
+++ src/Pages/Mentors.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {Container ,Card, Col, Row, Button} from 'react-bootstrap';  
 import firestoreDB from '../firebase/firestore.config';
 import { Link } from "react-router-dom";
 
@@ -28,25 +29,39 @@ function Mentors() {
     const resp = await firestoreDB.addDoc('mentorship', {first_name, last_name, bio});
     console.log(resp);
   };
+  
   return (
     <div>
-      <h3>Find a Mentor</h3>
-      <p>Become a Mentor</p>
-      <form onSubmit={submit}>
-        <input id="first_name" onChange={(event) => setFirstName(event.target.value)} placeholder="First Name"/>
-        <input id="last_name" onChange={(event) => setLastName(event.target.value)} placeholder="Last Name"/>
-        <textarea id="bio" onChange={(event) => setBio(event.target.value)} placeholder='Bio'></textarea>
-        <button type="submit">Submit</button>
-      </form>
-      <h4>Mentors</h4>
-      <ul>
-        {
-          viewableMentors.length > 0 &&
-          viewableMentors?.map((doc, i) => (
-            <li key={i}><Link to="/mentors">{doc.data.first_name}</Link></li>
-          ))
-        }
-      </ul>
+        <h3>Find a Mentor</h3>
+        <p>Become a Mentor</p>
+        <form onSubmit={submit}>
+            <input id="first_name" onChange={(event) => setFirstName(event.target.value)} placeholder="First Name"/>
+            <input id="last_name" onChange={(event) => setLastName(event.target.value)} placeholder="Last Name"/>
+            <textarea id="bio" onChange={(event) => setBio(event.target.value)} placeholder="Bio"></textarea>
+            <button type="submit">Submit</button>
+        </form>
+
+        <Container className='p-4'>
+            <Row lg={3}>
+            {
+            viewableMentors.length > 0 &&
+            viewableMentors?.map((doc, i) => (
+                // <li key={i}><Link to="/mentors">{doc.data.first_name}</Link></li>
+                <Col className="d-flex">
+                    <Card className="flex-fill" key={i}>
+                        <Card.Img variant="top" src={"#"} />
+                        <Card.Body>
+                        <Card.Title>{doc.data.first_name} {doc.data.last_name}</Card.Title>
+                        <Card.Text>{doc.data.job_title}</Card.Text>
+                        
+                        <Button variant="primary">Connect</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))
+            }
+            </Row>
+        </Container>
     </div>
   )
 }
