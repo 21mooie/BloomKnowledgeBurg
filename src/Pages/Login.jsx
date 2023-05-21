cd034215689a4c5fcc7f358b0982761b63974de6
twu361 <twu361@bloomberg.net>
2023-05-20 22:03:24 -0400

    small change2

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,25 +2,29 @@ import React, { useState } from "react";
 import { auth } from "../firebase/firebase.config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import jsCookie from 'js-cookie'
+import jsCookie from "js-cookie";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const user = await signInWithEmailAndPassword(auth, email, password);
-    if (user){
-      console.log(user)
-      jsCookie.set('token', user.user.accessToken)
-      navigate('/')
-    };
+    if (user) {
+      console.log(user);
+      jsCookie.set("token", user.user.accessToken);
+      navigate("/");
+    }
   };
 
   return (
     <div className="d-flex justify-content-center align-items-center mt-5 ">
-      <form className=" w-50 container shadow p-4 rounded" onSubmit={handleSubmit}>
+      <form
+        className=" w-50 container shadow p-4 rounded"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-3">
+          <h3 style={{ textAlign: "center" }}>Sign In</h3>
           <label htmlFor="email" className="form-label">
             Email address
           </label>
